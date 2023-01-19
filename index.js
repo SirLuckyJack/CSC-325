1ed0323a25cd366a615a314a79cb11195f93eeea
Jack <jacgoyet@umich.edu>
2023-01-19 16:29:13 -0500

    Added Discord Bot Files

---
+++ index.js
@@ -0,0 +1,67 @@
+const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions } = require('discord.js');
+
+const prefix = '/';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on("ready", () => {
+    console.log("Bot is online!");
+
+    client.user.setActivity('Barking at Email-Man', {type: "Barking"})
+})
+
+client.on("messageCreate", (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    //message array
+
+    const messageArray = message.content.split(" ");
+    const argument = messageArray.slice(1);
+    const cmd = messageArray[0];
+
+    //COMMANDS
+
+//test command
+
+if (command === 'test') {
+    message.channel.send("Bot is working!")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
